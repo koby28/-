1ddbf62bcf1d3c6d3b6a7c9a427d8d9a375c882c
koby28 <koby2828@126.com>
2018-07-18 04:24:32 -0400

    监控DOS攻击

---
+++ ddos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#解决DOS攻击生产
+log=/tmp/tmp.log
+[ -f $log ] || touch $log
+function add_iptables(){
+    while read line
+        do
+        ip=`echo $line | awk '{ print $2 }'`
+        count=`echo $line | wc -l`
+            if [ $count -gt 100 ] && [ `iptables -L -n | grep "$ip" | wc -l` -lt 1 ]
+            then
+                iptables -I INPUT -s $ip -j DROP
+                echo "$line isdropped" >> /tmp/droplist.log
+            fi
+        done<$log
+}
+
+function main(){
+    while true
+    do
+        netstat -an | grep EST | awk '{ print $(NF-1)}' | awk -F '[:]' '{ print $1 }' | sort | uniq -c >$log
+        add_iptables
+        sleep 180
+    done
+}
+
+main
